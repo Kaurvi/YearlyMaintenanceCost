3162b03076da623b8fc8fbfbe01cfcbbd0d8609a
Kaurvi <kaurvi@mail.uc.edu>
2023-06-04 20:09:18 -0400

    code

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,12 @@
+public class YearlyMaintenanceCost
+{
+    public static void main(String[] args)
+    {
+        int springCost = 100;
+        int winterCost = 150;
+        int summerCost = 200;
+        int fallCost = 300;
+        int totalCost = springCost + winterCost + fallCost + summerCost;
+        System.out.println("the total yearly maintenance cost" + totalCost );
+    }
+}
